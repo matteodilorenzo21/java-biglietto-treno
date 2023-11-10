b4f79d1c115f40e20f3759575191258612c1d3ae
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-11-10 16:06:28 +0100

    first commit

---
+++ src/org/lessons/java/Main.java
@@ -0,0 +1,33 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        double pricePerKm = 0.21;
+        double discountUnder = 0.8;
+        double discountOver = 0.6;
+
+        System.out.println("Distanza da percorrere: ");
+        double distanceKm = in.nextDouble();
+
+        System.out.println("Età del passeggero: ");
+        int age = in.nextInt();
+
+        double totalPrice = pricePerKm * distanceKm;
+
+        if (age < 18) {
+            totalPrice *= discountUnder;
+        } else if (age >= 65) {
+            totalPrice *= discountOver;
+        }
+
+        System.out.println("Il prezzo totale del viaggio è: " + totalPrice + "€");
+
+    }
+}
+
